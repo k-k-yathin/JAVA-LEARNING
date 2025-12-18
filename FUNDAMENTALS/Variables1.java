87f5dfead5e729ef05d1d4724af6ae02990a933f
k-k-yathin <yathinkarnamkiran@gmail.com>
2025-12-18 12:20:44 +0530

    CREATE Variables1.java

---
+++ FUNDAMENTALS/Variables1.java
@@ -0,0 +1,16 @@
+public class Variables1 {
+    public static void demo_class() {
+        int k = 10;
+        System.out.println(k);
+    }
+    public static void main(String[] args) {
+              // this is about variables
+        // there are two types : primitive and non_primitive (user defined)
+        int value; //declaration
+        value = 1;//initialization
+
+        int k = 15;
+        demo_class(); // we sill get the value of 10 as the output  we can just change the variable in the main method
+
+        }
+}
