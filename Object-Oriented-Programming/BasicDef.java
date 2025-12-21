51c41f59f7f89285723fc7ba07dae4dfc0c6ca57
k-k-yathin <yathinkarnamkiran@gmail.com>
2025-12-21 15:38:59 +0530

    Create BasicDef.java

---
+++ Object-Oriented-Programming/BasicDef.java
@@ -0,0 +1,16 @@
+public class OOPS1 {
+    public static void main(String[] args) {
+        // OOP is about creating objects that contain both data and methods
+        //-Abstraction
+        //-encapsulation
+        //-inheritance
+        //-polymorphism
+
+        //class
+        // classes are the blueprint from which objects are created.
+        //class can contain fields,methods,constructors,blocks and Nested class and interface
+
+        //objects
+        //a physical entity and a fundamental building block of object-oriented programming that represents a real-world entity with a specific state, behavior, and unique identity
+    }
+}
