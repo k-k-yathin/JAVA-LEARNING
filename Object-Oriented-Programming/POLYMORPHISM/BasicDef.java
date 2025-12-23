ca243f6bf35654a39f2846d3b7a8c92eac2cb030
k-k-yathin <yathinkarnamkiran@gmail.com>
2025-12-23 15:12:56 +0530

    Create BasicDef.java

---
+++ Object-Oriented-Programming/POLYMORPHISM/BasicDef.java
@@ -0,0 +1,10 @@
+public class BasicDef {
+    public static void main(String[] args) {
+        // a concept by which we can perform a single action in different ways
+        // two ways :
+        // method overloading (compile time )
+        // method overriding (run time) : method must have same access modifier
+        
+
+    }
+}
