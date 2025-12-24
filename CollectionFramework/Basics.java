6f519224b0d25041da9c02d32669477da18c81fa
k-k-yathin <yathinkarnamkiran@gmail.com>
2025-12-24 11:03:07 +0530

    Create Basics.java

---
+++ CollectionFramework/Basics.java
@@ -0,0 +1,9 @@
+public class CollectionFramework {
+    public static void main(String[] args) {
+        // provides an architecture to store and manipulate a group of objects
+        // reduces the effort required to write the code by providing dsa
+        // provides high-performance and high-quality data structures thereby increasing the speed
+        // -interfaces , -classes , -Algorithm
+
+    }
+}
